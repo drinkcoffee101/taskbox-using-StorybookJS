644e2ebc7d55de3593788e35cc763aa117552bfc
drinkcoffee <dcoffie92@gmail.com>
2020-01-09 06:21:50 -0800

    longtitle

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -2,6 +2,7 @@ import React from "react";
 // 'action' -  allows us to create a callback that appears in the actions panel of the Storybook UI when clicked
 // help you verify interactions when building UI components in isolation.
 import { action } from "@storybook/addon-actions";
+// easy access to editing passed data to a component
 import { withKnobs, object } from "@storybook/addon-knobs/react";
 
 import Task from './Task'
@@ -27,9 +28,15 @@ export const actionsData = {
     onArchiveTask: action('onArchiveTask')
 }
 
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
 //Story 1
 export const Default = () => <Task task={object('task', { ...taskData })} {...actionsData} />
 //Story 2
 export const Pinned = () => <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />
 //Story 3
 export const Archived = () => <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+
+export const LongTitle = () => (
+    <Task task={{ ...taskData, title: longTitleString }} {...actionsData} />
+)
