a9813cc308838b7c481ac3022af4a658de9bad2a
drinkcoffee <dcoffie92@gmail.com>
2020-01-09 18:39:09 -0800

    hi

---
+++ .storybook/addons/design-assets.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+import { AddonPanel } from "@storybook/components";
+import { addons, types } from "@storybook/addons";
+import { useParameter } from "@storybook/api"
+
+const Content = () => {
+    // story's parameter being retrieved here
+    const results = useParameter("assets", [])
+
+    return (
+        <Fragment>
+            {results.length ? (
+                <ol>
+                    {results.map(i => (
+                        <li>{i}</li>
+                    ))}
+                </ol>
+            ) : null}
+        </Fragment>
+    )
+}
+
+addons.register("my/design-assets", () => {
+    addons.add("design-assets/panel", {
+        title: "assets",
+        type: types.PANEL,
+        render: ({ active, key }) => (
+            <AddonPanel active={active} key={key}>
+                <Content />
+            </AddonPanel>
+        )
+    })
+})
